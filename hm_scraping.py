de9b1c9975b08f01aeb05a2bfe293b15462d2b50
hmoritaguidewire <himorita.aug@gmail.com>
2017-02-22 00:56:03 +0900

    use requests to login

--- hm_scraping.py
+++ hm_scraping.py
@@ -1,15 +1,27 @@
 import sys
 import os
-from robobrowser import RoboBrowser
+import requests
+import lxml.html
 from bs4 import BeautifulSoup
 import urllib.request
 
-url = 'https://mynichinoken.jp/mynichinoken/index.php'
-#url = 'https://www.google.co.jp/'
-#browser = RoboBrowser(parser='html.parser')
-#browser.open(url)  # open()メソッドでGoogleのトップページを開く。
+#url = 'https://mynichinoken.jp/mynichinoken/index.php'
+url = 'https://mynichinoken.jp/mynichinoken/login/mns0101_01.php'
+student_url = 'https://mynichinoken.jp/mynichinoken/student/index.php'
+session = requests.Session()
+headers = {'User-Agent': 'Mozilla/5.0'}
+data = {'id':'N1207585','passwd':'keigo0604'}
+session.post(url,headers=headers, data=data)
+s = session.get(student_url)
+print(s.content)
+root = lxml.html.fromstring(s.content)
+print(root)
 
-html = urllib.request.urlopen(url).read()
-print(html)
-soup = BeautifulSoup(html, 'lxml')
-print(soup)
+#html = urllib.request.urlopen(url).read()
+#print(html)
+#soup = BeautifulSoup(html, 'lxml')
+#print(soup)
+#for field in soup.find_all(name='input'):
+#    print(field)
+#    n = field.get('name')
+#    print(n)
